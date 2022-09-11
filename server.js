9f673f378a40629d697d67defffc217e3d77d2c6
Jaffer Veronezi <veronezitecnologia@gmail.com>
2022-09-11 16:50:09 -0300

    Adiciona lista de produtos

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const express = require ('express')
 const cors = require('cors');
 const path = require ('path')
+const apiRouter = require('./api/routes/apiRouter')
 const app = express ()
+
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/app', express.static (path.join (__dirname, '/public')))
+app.use ('/api', apiRouter)
+
 
 let port = process.env.PORT || 3000
 app.listen (port) 
