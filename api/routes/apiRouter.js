89c7285b42454117856f87ac4e4f1f5e2169e8f3
Jaffer Veronezi <veronezitecnologia@gmail.com>
2022-09-19 20:26:29 -0300

    adiciona conexao e metodos pra banco de dados

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -4,6 +4,14 @@ let apiRouter = express.Router()
 
 const endpoint = '/'
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    }
+   });
+
 const lista_produtos = {
  produtos: [
  { id: 1, descricao: "Produto 1", valor: 5.00, marca: "marca " },
@@ -15,33 +23,78 @@ const lista_produtos = {
 }
 
 apiRouter.get (endpoint + 'produtos', function (req, res) {
- res.status(200).json (lista_produtos)
+ //res.status(200).json (lista_produtos)
+ knex
+     .select ('*')
+     .from ('produto')
+     .then (results => {
+        res.status(200).json(results)
+     })
+     .catch(err => {
+        res.status(500).json ({ message: `Erro ao obter produtos: ${err.message}` })
+     })
 })
 
 apiRouter.get(endpoint + 'produtos/:id', (req, res) => {
-    let id = parseInt(req.params.id)
-    let idx = lista_produtos.produtos.findIndex (elem => elem.id === id)
+    
+    // let id = parseInt(req.params.id)
+    // let idx = lista_produtos.produtos.findIndex (elem => elem.id === id)
 
-    if (idx == -1) {
-        res.status(404).json ({message: `Produto nao encontrado. ID: ${id}`})
-    } else {
-        res.status(200).json(lista_produtos.produtos[idx])
-    }
+    // if (idx == -1) {
+    //     res.status(404).json ({message: `Produto nao encontrado. ID: ${id}`})
+    // } else {
+    //     res.status(200).json(lista_produtos.produtos[idx])
+    // }
+
+    knex
+    .select ('*')
+    .from ('produto')
+    .then (results => {
+       if (results.length) {
+        let produto = results[0]
+        res.status(200).json(produto)
+       } else {
+        res.status(404).json({message: `Produto nao encotnrado`})
+       }
+    })
+    .catch(err => {
+       res.status(500).json ({ message: `Erro ao obter produtos: ${err.message}`})
+    })
+
 })
 
 apiRouter.post(endpoint + 'produtos', express.json(), (req, res) => {
+    // if (req.body) {
+    //     let newId = Math.max.apply(Math, lista_produtos.produtos.map(function(o) {return o.id; }))
+    //     let produto = req.body
+    //     produto.id = newId + 1
+    //     lista_produtos.produtos.push (produto)
+
+    //     res.status(201).json({
+    //         message: ` Produto incluido com sucesso`,
+    //         id: produto.id
+    //     })
+
+    // }
+
     if (req.body) {
-        let newId = Math.max.apply(Math, lista_produtos.produtos.map(function(o) {return o.id; }))
-        let produto = req.body
-        produto.id = newId + 1
-        lista_produtos.produtos.push (produto)
-
-        res.status(201).json({
-            message: ` Produto incluido com sucesso`,
-            id: produto.id
+        knex ('produto')
+        .insert({
+            descricao: req.body.descricao,
+            valor: req.body.valor,
+            marca: req.marca
+        }, ['id'])
+        .then((result) => {
+            let usuario = result[0]
+            res.status(200).json({"id": usuario.id })
+        return
         })
-
+        .catch(err => {
+            res.status(500).json({
+            message: 'Erro ao registrar usuario - ' + err.message })
+        })
     }
+
 })
 
 apiRouter.delete(endpoint + 'produtos/:id', (req, res) => {
